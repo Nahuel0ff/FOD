99235ac53c3cb288e2b8d4935b98465a899e8ba0
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2025-04-12 18:07:46 -0300

    Update ejercicio2.pas

--- TP's/TP2/ejercicio2.pas
+++ TP's/TP2/ejercicio2.pas
@@ -28,14 +28,14 @@ type
 	procedure leerProd (var p:prod);
 	begin
 		writeln('Ingrese el precio, para salir -1');
-		readln(p.precio)
+		readln(p.precio);
 		if (p.precio <> -1) then begin
 			writeln('Ingrese el codigo de prod: ');
-			readln(e.cod);
+			readln(p.cod);
 			writeln('Ingrese el nombre del prod: ');
-			readln(e.nom);
+			readln(p.nom);
 			writeln('Ingrese el stock actual del prod: ');
-			readln(e.stockact);
+			readln(p.stockact);
 			writeln('Ingrese el stock min del prod: ');
 			readln(p.stockmin);
 		end;
@@ -61,9 +61,10 @@ type
 	begin
 		writeln('Ingresa las unidades vendidas, -1 para salir');
 		readln(v.univend);
-		if (v.univend <> -1) then
+		if (v.univend <> -1) then begin
 			writeln('Ingrese el codigo del prod vendido');
 			readln(v.cod);
+		end;
 	end;
 
 	procedure cargarDetalle(var d:detalle);
@@ -82,8 +83,89 @@ type
 	end;
 	
 // Punto A
+	procedure leerDetalle (var d:detalle; var v:venta);
+	begin
+		if not EOF(d) then
+			read(d,v)
+		else
+			v.cod:=valorAlto;
+	end;
 	
+	procedure modificarProd(var prod_nue:prod; c:integer; produ:prod);
+	begin
+		prod_nue.cod:=produ.cod;
+		prod_nue.precio:=produ.precio;
+		prod_nue.nom:=produ.nom;
+		prod_nue.stockact:=produ.stockact + c;
+		prod_nue.stockmin:=produ.stockmin;
+	end;
+
+	procedure procesarYActulizar(var m:maestro; var d:detalle);
+	var
+		prod_act:prod; //Maestro
+		vent_act:venta;//Detalle
+	begin
+		reset(m);
+		reset(d);
+		
+		leerDetalle(d,vent_act);
+		
+		while (vent_act.cod <> valorAlto) do begin
+			read(m,prod_act);
+			while(prod_act.cod <> vent_act.cod) do
+				read(m,prod_act);                   //Busco en el maestro
+				
+			while (prod_act.cod = vent_act.cod) do begin
+				prod_act.stockact:= prod_act.stockact - vent_act.univend;
+				leerDetalle(d,vent_act);
+			end;
+			seek(m,filepos(m)-1);
+			write(m,prod_act);                      
+		end;
+		close(d);
+		close(m);
+	end;
 	
+	procedure listarStockMinimo(var m: maestro);
+	var
+		p: prod;
+		txt: text;
+	begin
+		assign(m, 'maestro.dat');
+		reset(m);
+		assign(txt, 'stock_minimo.txt');
+		rewrite(txt);
+	
+		while not eof(m) do begin
+			read(m, p);
+			if (p.stockact < p.stockmin) then
+				writeln(txt, 'Codigo: ', p.cod, ' Nombre: ', p.nom, ' Stock actual: ', p.stockact, ' Stock minimo: ', p.stockmin);
+		end;
+	
+		close(m);
+		close(txt);
+	end;
+	
+// Para corroborar
+
+procedure imprimirMaestro(var m: maestro);
+var
+	p: prod;
+begin
+	reset(m);
+	writeln('----------------------------------------');
+	while not eof(m) do begin
+		read(m, p);
+		writeln('Código: ', p.cod);
+		writeln('Nombre: ', p.nom);
+		writeln('Precio: $', p.precio:0:2);
+		writeln('Stock actual: ', p.stockact);
+		writeln('Stock mínimo: ', p.stockmin);
+		writeln('----------------------------------------');
+	end;
+	close(m);
+end;
+
 
 	
 //Programa principal
@@ -91,10 +173,14 @@ var
 	m:maestro;
 	d:detalle;
 BEGIN
-	assign(m,'Maestro');
-	assign(d,'Detalle');
+	assign(m,'maestro.dat');
+	assign(d,'detalle.dat');
 	cargarMaestro(m);
 	cargarDetalle(d);
-	
+	writeln('Listado del archivo maestro sin actualizar:');
+	imprimirMaestro(m);
+	procesarYActulizar(m,d);
+	writeln('Listado del archivo maestro actualizado:');
+	imprimirMaestro(m);
+	listarStockMinimo(m);
 END.
-
